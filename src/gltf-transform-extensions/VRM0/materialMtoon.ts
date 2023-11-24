b1594cf924d08ef269fb24ef697371288e301800
miramocha <96218694+miramocha@users.noreply.github.com>
2023-11-24 01:14:14 -0500

    temp

--- src/gltf-transform-extensions/VRM0/materialMtoon.ts
+++ src/gltf-transform-extensions/VRM0/materialMtoon.ts
@@ -10,7 +10,7 @@ import {
 } from "@gltf-transform/core";
 import { VRM0 } from "./constants.ts";
 
-const { R, G, B, A } = TextureChannel;
+const { R, G, B } = TextureChannel;
 
 const NAME = VRM0; // CHANGE THIS FOR VRM1
 
@@ -26,11 +26,6 @@ export interface IMaterialMToon extends IProperty {
   /**
    * Lighting
    */
-  // litColorFactor: vec3; - VRM0
-  // litTexture: Texture; - VRM0
-  // litTextureInfo: TextureInfo - VRM0
-  // normalTexture: Texture; - VRM0
-  // normalTextureInfo: TextureInfo - VRM0
   shadeColorFactor: vec3;
   shadeMultiplyTexture: Texture;
   shadeMultiplyTextureInfo: TextureInfo;
@@ -95,11 +90,6 @@ export default class MaterialMToon extends ExtensionProperty<IMaterialMToon> {
       /**
        * Lighting
        */
-      // litColorFactor: vec3; - VRM0
-      // litTexture: Texture; - VRM0
-      // litTextureInfo: TextureInfo - VRM0
-      // normalTexture: Texture; - VRM0
-      // normalTextureInfo: TextureInfo - VRM0
       shadeColorFactor: [0, 0, 0],
       shadeMultiplyTexture: null,
       shadeMultiplyTextureInfo: new TextureInfo(
@@ -157,4 +147,214 @@ export default class MaterialMToon extends ExtensionProperty<IMaterialMToon> {
       uvAnimationRotationSpeedFactor: 0.0,
     });
   }
+
+  public getSpecVersion(): "1.0" | "1.0-beta" {
+    return this.get("specVersion");
+  }
+  public setSpecVersion(specVersion: "1.0" | "1.0-beta"): this {
+    return this.set("specVersion", specVersion);
+  }
+
+  public getTransparentWithZWrite(): boolean {
+    return this.get("transparentWithZWrite");
+  }
+  public setTransparentWithZWrite(transparentWithZWrite: boolean): this {
+    return this.set("transparentWithZWrite", transparentWithZWrite);
+  }
+
+  public getRenderQueueOffsetNumber(): number {
+    return this.get("renderQueueOffsetNumber");
+  }
+  public setRenderQueueOffsetNumber(renderQueueOffsetNumber: number): this {
+    return this.set("renderQueueOffsetNumber", renderQueueOffsetNumber);
+  }
+
+  /**********************************************************************************************
+   * Lighting
+   */
+  public getShadeColorFactor(): vec3 {
+    return this.get("shadeColorFactor");
+  }
+  public setShadeColorFactor(shadeColorFactor: vec3): this {
+    return this.set("shadeColorFactor", shadeColorFactor);
+  }
+
+  public getShadeMultiplyTexture(): Texture | null {
+    return this.getRef("shadeMultiplyTexture");
+  }
+  public setShadeMultiplyTextureInfo(texture: Texture | null): this {
+    return this.setRef("shadeMultiplyTexture", texture, {
+      channels: R | G | B,
+    });
+  }
+  public getShadeMultiplyTextureInfo(): TextureInfo | null {
+    return this.getRef("shadeMultiplyTexture")
+      ? this.getRef("shadeMultiplyTextureInfo")
+      : null;
+  }
+
+  public getShadingShiftFactor(): number {
+    return this.get("shadingShiftFactor");
+  }
+  public setShadingShiftFactor(shadingShiftFactor: number): this {
+    return this.set("shadingShiftFactor", shadingShiftFactor);
+  }
+
+  public getShadingShiftTexture(): Texture | null {
+    return this.getRef("shadingShiftTexture");
+  }
+  public setShadingShiftTextureInfo(texture: Texture | null): this {
+    return this.setRef("shadingShiftTexture", texture, {
+      channels: R | G | B,
+    });
+  }
+  public getShadingShiftTextureInfo(): TextureInfo | null {
+    return this.getRef("shadingShiftTexture")
+      ? this.getRef("shadingShiftTextureInfo")
+      : null;
+  }
+
+  public getShadingToonyFactor(): number {
+    return this.get("shadingToonyFactor");
+  }
+  public setShadingToonyFactor(shadingToonyFactor: number): this {
+    return this.set("shadingToonyFactor", shadingToonyFactor);
+  }
+
+  public getGIEqualizationFactor(): number {
+    return this.get("giEqualizationFactor");
+  }
+  public setGIEqualizationFactor(giEqualizationFactor: number): this {
+    return this.set("giEqualizationFactor", giEqualizationFactor);
+  }
+
+  /**********************************************************************************************
+   * Rim
+   */
+  public getMatcapFactor(): vec3 {
+    return this.get("matcapFactor");
+  }
+  public setMatcapFactor(matcapFactor: vec3): this {
+    return this.set("matcapFactor", matcapFactor);
+  }
+
+  public getMatcapTexture(): Texture | null {
+    return this.getRef("matcapTexture");
+  }
+  public setMatcapTextureInfo(texture: Texture | null): this {
+    return this.setRef("matcapTexture", texture, {
+      channels: R | G | B,
+    });
+  }
+  public getMatcapTextureInfo(): TextureInfo | null {
+    return this.getRef("matcapTexture")
+      ? this.getRef("matcapTextureInfo")
+      : null;
+  }
+
+  public getParametricRimColorFactor(): vec3 {
+    return this.get("parametricRimColorFactor");
+  }
+  public setParametricRimColorFactor(parametricRimColorFactor: vec3): this {
+    return this.set("parametricRimColorFactor", parametricRimColorFactor);
+  }
+
+  public getRimMultiplyTexture(): Texture | null {
+    return this.getRef("rimMultiplyTexture");
+  }
+  public setRimMultiplyTextureInfo(texture: Texture | null): this {
+    return this.setRef("rimMultiplyTexture", texture, {
+      channels: R | G | B,
+    });
+  }
+  public getRimMultiplyTextureInfo(): TextureInfo | null {
+    return this.getRef("rimMultiplyTexture")
+      ? this.getRef("rimMultiplyTextureInfo")
+      : null;
+  }
+
+  public getRimLightningMixFactor(): number {
+    return this.get("rimLightingMixFactor");
+  }
+  public setRimLightningMixFactor(rimLightningMixFactor: number): this {
+    return this.set("rimLightingMixFactor", rimLightningMixFactor);
+  }
+
+  public getParametricRimFresnelPowerFactor(): number {
+    return this.get("parametricRimFresnelPowerFactor");
+  }
+  public setParametricRimFresnelPowerFactor(
+    parametricRimFresnelPowerFactor: number
+  ): this {
+    return this.set(
+      "parametricRimFresnelPowerFactor",
+      parametricRimFresnelPowerFactor
+    );
+  }
+
+  public getParametricRimLiftFactor(): number {
+    return this.get("parametricRimLiftFactor");
+  }
+  public setParametricRimLiftFactor(parametricRimLiftFactor: number): this {
+    return this.set("parametricRimLiftFactor", parametricRimLiftFactor);
+  }
+
+  /**********************************************************************************************
+   * Outline
+   */
+  public getOutlineWidthMode():
+    | "none"
+    | "worldCoordinates"
+    | "screenCoordinates" {
+    return this.get("outlineWidthMode");
+  }
+  public setOutlineWidthMode(
+    outlineWidthMode: "none" | "worldCoordinates" | "screenCoordinates"
+  ) {
+    return this.set("outlineWidthMode", outlineWidthMode);
+  }
+
+  public getOutlineWidthFactor(): number {
+    return this.get("outlineWidthFactor");
+  }
+  public setOutlineWidthFactor(outlineWidthFactor: number): this {
+    return this.set("outlineWidthFactor", outlineWidthFactor);
+  }
+
+  public getOutlineWidthMultiplyTexture(): Texture | null {
+    return this.getRef("outlineWidthMultiplyTexture");
+  }
+  public setOutlineWidthMultiplyTextureInfo(texture: Texture | null): this {
+    return this.setRef("outlineWidthMultiplyTexture", texture, {
+      channels: R | G | B,
+    });
+  }
+  public getOutlineWidthMultiplyTextureInfo(): TextureInfo | null {
+    return this.getRef("outlineWidthMultiplyTexture")
+      ? this.getRef("outlineWidthMultiplyTextureInfo")
+      : null;
+  }
+
+  public getOutlineColorFactor(): vec3 {
+    return this.get("outlineColorFactor");
+  }
+  public setOutlineColorFactor(outlineColorFactor: vec3): this {
+    return this.set("outlineColorFactor", outlineColorFactor);
+  }
+
+  public getOutlineLightningMixFactor(): number {
+    return this.get("outlineLightingMixFactor");
+  }
+  public setOutlineLightningMixFactor(outlineLightningMixFactor: number): this {
+    return this.set("outlineLightingMixFactor", outlineLightningMixFactor);
+  }
+
+  /**********************************************************************************************
+   * UV Animation
+   */
+  // uvAnimationMaskTexture: Texture;
+  // uvAnimationMaskTextureInfo: TextureInfo;
+  // uvAnimationScrollXSpeedFactor: number;
+  // uvAnimationScrollYSpeedFactor: number;
+  // uvAnimationRotationSpeedFactor: number;
 }
