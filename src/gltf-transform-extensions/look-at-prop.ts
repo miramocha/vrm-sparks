6267a3e7d9add13e69829062e0c4204cf77371b4
miramocha <96218694+miramocha@users.noreply.github.com>
2023-11-28 03:56:32 -0500

    start lookat prop

--- src/gltf-transform-extensions/look-at-prop.ts
+++ src/gltf-transform-extensions/look-at-prop.ts
@@ -1,23 +1,16 @@
 import { IProperty, Nullable, Property } from "@gltf-transform/core";
-import { HumanoidHumanBoneProp } from "./humanoid-human-bone-prop.ts";
 import { PropertyType as VRMPropertyType } from "./constants.ts";
 
-export interface IHumanoidProp extends IProperty {
-  humanoidHumanBonePropMap: {
-    [key: string]: HumanoidHumanBoneProp;
-  };
-}
+export interface ILookAtProp extends IProperty {}
 
-export class HumanoidProp extends Property<IHumanoidProp> {
+export class LookAtProp extends Property<ILookAtProp> {
   public declare propertyType: VRMPropertyType.LOOK_AT_PROP;
 
   protected init(): void {
     this.propertyType = VRMPropertyType.LOOK_AT_PROP;
   }
 
-  protected getDefaults(): Nullable<IHumanoidProp> {
-    return Object.assign(super.getDefaults() as IProperty, {
-      humanoidHumanBonePropMap: {},
-    });
+  protected getDefaults(): Nullable<ILookAtProp> {
+    return Object.assign(super.getDefaults() as IProperty, {});
   }
 }
